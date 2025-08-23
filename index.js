3c3f6f41a2a3189a584651141da06f11e74e7def
Sanjay Singh <sanjay007abd@gmail.com>
2025-08-23 17:12:23 +0530

    add jwt and url short code

--- index.js
+++ index.js
@@ -1,15 +1,18 @@
+import 'dotenv/config'
 import express from 'express';
-
+import { authenticationMiddleware } from './middlewares/auth.middleware.js';
 import router from './routes/user.routes.js'
+import urlRouter from './routes/url.routes.js';
 
 const app = express()
 
 const PORT = process.env.PORT ?? 8000
 
 app.use(express.json())
+app.use(authenticationMiddleware)
 
 app.use('/user', router)
-
+app.use(urlRouter)
 app.get('/', (req,res) => {
     return res.json({status: 'Server is up and running...'});
 })
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,10 @@
 import express from 'express';
 import db from '../db/index.js'
 import {usersTable} from '../models/index.js'
-import { eq } from 'drizzle-orm';
-import { randomBytes, createHmac } from 'crypto';
-import {signupPostRequestBodySchema} from '../validation/request.validation.js'
+import {hashPasswordWithSalt} from '../utils/hash.js'
+import {getUserByEmail} from '../services/user.service.js'
+import {signupPostRequestBodySchema, loginPostRequestBodySchema} from '../validation/request.validation.js'
+import { createUserToken } from '../utils/token.js';
 
 const router = express.Router()
 
@@ -16,18 +17,13 @@ router.post('/signup',async (req,res) => {
 
     const {firstname, lastname, email, password } = validationResult.data;
 
-    const [existingUser] = await db
-    .select({
-        id: usersTable.id,
-    })
-    .from(usersTable)
-    .where(eq(usersTable.email, email))
+    const existingUser = await getUserByEmail(email)
 
     if(existingUser)
         return res.status(400).json({error: `User with email ${email} already exists!`});
 
-    const salt = randomBytes(256).toString('hex')
-    const hashedPassword = createHmac('sha256',salt).update(password).digest('hex')
+    
+    const {salt, password:hashedPassword }  = hashPasswordWithSalt(password)
 
     const [user] = await db.insert(usersTable).values({
         email,
@@ -40,6 +36,37 @@ router.post('/signup',async (req,res) => {
 
     return res.status(201).json({data: {userId: user.id}})
 
+
+})
+
+router.post('/login', async (req,res)=>{
+    const validationResult = await loginPostRequestBodySchema.safeParseAsync(req.body)
+
+    if(validationResult.error) {
+        return res.status(400).json({error: validationResult.error})
+    }
+
+    const {email,password} = validationResult.data;
+
+    const user = await getUserByEmail(email)
+
+    if(!user){
+        return res.status(404).json({error: `User with email ${email} does not exist`})
+    }
+
+    const {password: hashedPassword} = hashPasswordWithSalt(
+        password,
+        user.salt
+    )
+
+    if (user.password !== hashedPassword){
+        return res.status(400).json({error: 'Invalid Password'})
+    }
+
+    
+    const token = await createUserToken({id: user.id})
+
+    return res.json({token})
 
 })
 
